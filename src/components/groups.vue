c4fe0d5ed2b1e71f4566df32325ca8e5277ce53a
Raj Aryan Sharma <rajaryan.sharma14400@gmail.com>
2022-02-18 11:50:16 +0530

    groups.vue

---
+++ src/components/groups.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <form>
+            <input style="margin:auto; width: 220px; margin-bottom:10px; margin-top:50px;" v-model="gName" class="" type="text" placeholder="Group Name">
+            <br>
+            <button v-on:click.prevent="addGroup" style="margin:auto; margin-bottom:20px;">Add Group</button>
+        </form>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    name: 'todo-group',
+    data() {
+        return{
+            gName: '',
+            groups: [
+            // {
+            //   'id': 1,
+            //   'name': 'Tech',
+            //   'open': false,
+            // }
+            ],
+        }
+    },
+    methods: {
+        addGroup(){
+
+        },
+    },
+}
+</script>
+
+<style>
+
+</style>
