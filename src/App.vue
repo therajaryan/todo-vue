894bbf952f0a0d47615966d9c48093f60293f683
Raj Aryan Sharma <rajaryan.sharma14400@gmail.com>
2022-02-18 11:37:54 +0530

    groups

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img height="60px" src="./assets/unnamed.png">
+    <groups></groups>
     <todo></todo>
   </div>
 </template>
@@ -8,20 +9,20 @@
 <script>
 import HelloWorld from './components/HelloWorld'
 import todo from './components/todo'
+import groups from './components/groups.vue'
+import Groups from './components/groups.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
+    groups,
     todo,
   }
 }
 </script> 
 
 <style>
-
-
- 
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
